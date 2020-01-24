f66a94d32db0a83f5e55185580035251d12057b1
yanhao gong <yanhao.gong@mail.mcgill.ca>
2020-01-24 14:16:26 -0500

    Updates

---
+++ precache-manifest.717dd5b46986b89f084ee43eb8467946.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86c946febbcdd91a5d040c5b375beb9a",
+    "url": "/yhgong/index.html"
+  },
+  {
+    "revision": "38c2e88572d8457e246b",
+    "url": "/yhgong/static/js/2.aa970b11.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/yhgong/static/js/2.aa970b11.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45d225b657d45858499a",
+    "url": "/yhgong/static/js/main.bbfd26a0.chunk.js"
+  },
+  {
+    "revision": "0ce181833d342edd589c",
+    "url": "/yhgong/static/js/runtime-main.ec3d4ec0.js"
+  },
+  {
+    "revision": "6e1cf0ed589a00a657c0789455be7fed",
+    "url": "/yhgong/static/media/contact-mobile.6e1cf0ed.jpg"
+  },
+  {
+    "revision": "9792880c60ba8067fdcc8bebdd4171d7",
+    "url": "/yhgong/static/media/contact.9792880c.jpg"
+  },
+  {
+    "revision": "faa2daffa01110a9c6c5a4637538ab41",
+    "url": "/yhgong/static/media/edu-mobile.faa2daff.jpg"
+  },
+  {
+    "revision": "1fc8f612b0e8a6e5141b5c4afef94200",
+    "url": "/yhgong/static/media/edu.1fc8f612.jpg"
+  },
+  {
+    "revision": "3bdf1a3e0f6be9715901f676d823a877",
+    "url": "/yhgong/static/media/me-mobile.3bdf1a3e.jpg"
+  },
+  {
+    "revision": "ffb70c886a33f105e64d158e67f9a057",
+    "url": "/yhgong/static/media/me.ffb70c88.jpg"
+  },
+  {
+    "revision": "ce6fa71281cabf8f88f3fbefa69d7286",
+    "url": "/yhgong/static/media/projects-mobile.ce6fa712.jpg"
+  },
+  {
+    "revision": "2fe898cb229ce95a151c319bd09ead8e",
+    "url": "/yhgong/static/media/projects.2fe898cb.jpg"
+  },
+  {
+    "revision": "9150f688b641e72be94ec88db6b32531",
+    "url": "/yhgong/static/media/self-mobile.9150f688.jpg"
+  },
+  {
+    "revision": "877796714fc15c6747df8eb9fb8fa15c",
+    "url": "/yhgong/static/media/self.87779671.jpg"
+  },
+  {
+    "revision": "d9a80570b1a9289fbc094768e2b8e7d2",
+    "url": "/yhgong/static/media/work.d9a80570.jpg"
+  }
+]);
